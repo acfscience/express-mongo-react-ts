fd9adf5c6634813bdfc75f392f8ece2e7bd2fb62
Arnaldo Carreno <acarreno@us.ibm.com>
2020-10-07 16:17:55 -0500

    Initial project setup with placeholder files

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import todoRoutes from "./routes";
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(todoRoutes);
+
+const uri: string = process.env.MONGO_URI || "";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.set("useFindAndModify", false);
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => {
+    throw error;
+  });
